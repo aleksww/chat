78b407c2cbac0650feaa312843f33145f73dc873
Алексей <aleks.woronow@yandex.ru>
2017-02-28 11:31:28 +0000

    Changed urls static

--- chat/urls.py
+++ chat/urls.py
@@ -35,5 +35,12 @@ urlpatterns = [
         name="login"),
     url(r'^logout/$', auth_views.logout,
         {'next_page': 'login'}, name="logout"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
