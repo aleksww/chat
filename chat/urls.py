4e85201818e8eba07b7198abbaa9fbb7c00d99a5
Алексей <aleks.woronow@yandex.ru>
2017-02-28 11:23:57 +0000

    Added runtime file

--- chat/urls.py
+++ chat/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
 
 
